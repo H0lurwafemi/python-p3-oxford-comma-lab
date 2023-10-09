c7e7e93e26127b69325b12dc92b484a58f2ba932
unknown <stephenoluwafemi52@gmail.com>
2023-10-09 12:55:36 +0100

    Done with assignment

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,23 @@
-def oxford_comma(items):
-    return None
+def oxford_comma(arr):
+    result = ""
+
+    # Check for empty array
+    if len(arr) == 0:
+        return result
+
+    # Handle single-element case
+    if len(arr) == 1:
+        return arr[0]
+
+    # Iterate through elements, adding commas and "and"
+    for i in range(len(arr) - 1):
+        result += arr[i] + ", "
+
+    # Add the last element with "and"
+    result += "and " + arr[-1]
+
+    return result
+
+
+
+    
